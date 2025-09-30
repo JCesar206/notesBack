5619b880ba5fd42e3aad67f38d82e4cd9ae0910d
JCesar206 <jcesar206@hotmail.com>
2025-09-30 10:01:06 -0600

    Se modifica archivo auth.middleware.js

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,25 +1,23 @@
-import { supabase } from "../db.js";
+import jwt from "jsonwebtoken";
 
-export const authenticate = async (req, res, next) => {
+export const authMiddleware = (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
-
+    const authHeader = req.headers["authorization"];
     if (!authHeader) {
-      return res.status(401).json({ error: "Token no proporcionado" });
+      return res.status(401).json({ error: "No autorizado. Falta token." });
     }
 
-    const token = authHeader.split(" ")[1]; // Bearer <token>..
-
-    // Obtener usuario desde el token
-    const { data, error } = await supabase.auth.getUser(token);
-
-    if (error || !data.user) {
-      return res.status(401).json({ error: "Token inválido o expirado" });
+    const token = authHeader.split(" ")[1]; // "Bearer <token>"
+    if (!token) {
+      return res.status(401).json({ error: "Token inválido o ausente." });
     }
 
-    req.user = data.user; // guardamos el usuario en la request
+    // Verificamos el token con la secret
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // guardamos los datos en la request
     next();
   } catch (err) {
-    res.status(401).json({ error: err.message });
+    console.error("Error en authMiddleware:", err.message);
+    return res.status(403).json({ error: "Token inválido o expirado." });
   }
 };
