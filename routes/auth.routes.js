5ebb29e61089d07a1ee043c367ab3b2bc9dfe393
JCesar206 <jcesar206@hotmail.com>
2025-09-30 12:46:44 -0600

    Se hacen ajustes en backend para manejo de errores y token

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,8 @@
-import { Router } from "express";
+import express from "express";
 import { register, login } from "../controllers/auth.controller.js";
 
-const router = Router();
+const router = express.Router();
 
-// Rutas de autenticación
 router.post("/register", register);
 router.post("/login", login);
 
