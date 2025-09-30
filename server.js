cd0ab500a9aa6ef31d85b7c6ea02dfe39184a79c
JCesar206 <jcesar206@hotmail.com>
2025-09-30 09:39:32 -0600

    Actualización en server

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`✅🚀 Server running on port ${PORT}`);
 });
