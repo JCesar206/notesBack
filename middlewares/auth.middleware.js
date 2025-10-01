0b9122be5efb3e10a3bbcaab03d897b3b692b796
JCesar206 <jcesar206@hotmail.com>
2025-10-01 17:39:55 -0600

    Se rehace el backend y se migra a un server por problemas de comunicación

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,18 +3,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const authMiddleware = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
+  const authHeader = req.headers.authorization;
   if (!authHeader) return res.status(401).json({ error: "No token provided" });
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   if (!token) return res.status(401).json({ error: "Token missing" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    req.userId = decoded.id;
     next();
   } catch (err) {
-    console.error("Error en authMiddleware:", err);
     return res.status(401).json({ error: "Invalid token" });
   }
 };
--- server.js
+++ server.js
@@ -7,11 +7,11 @@ import notesRoutes from "./routes/notes.routes.js";
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: "https://jcesar206.github.io/notesFront" })); // tu front
+app.use(cors({ origin: "https://jcesar206.github.io/notesFront" })); // URL de tu front
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", notesRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
